d4e4dd2b5bf8d70e983f290e662410b272093ccb
BBJayy <romaqpka9@gmail.com>
2018-02-04 14:22:03 +0200

    Initial commit

---
+++ Z#1: Perfect Hashing/main.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::endl;
+using std::fill;
+
+
+bool isPrime(int n)
+{
+    int sqrtn = sqrt(n);
+    for (int i = 2; i <= sqrtn; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    
+    return true;
+}
+
+//given a number n, find the next closest prime number above n
+int findNextPrime(int n)
+{
+    while (!isPrime(n)) {
+        n++;
+    }
+    return n;
+}
+
+
+
+int hash(int a, int k, int b, int bigPrime, int tableSize) {
+    return ((a*k + b) % bigPrime) % tableSize;
+}
+
+class Bucket {
+    int bigPrime = 1009;
+    int a = rand() % bigPrime, b = rand() % bigPrime;
+    vector<int> innerHashTable;
+    
+    
+public:
+    
+    size_t size() const{
+        return innerHashTable.size();
+    }
+    
+    void add(int elem){
+        // 1. double the capacity
+        // 2. rehash all items
+        // 3. place the item
+        // 3.1 if collision -> randomize a,b -> step 2
+        
+        // size == 0 or 1 -> size += 1 else size = size^2 so that probability of collision less then 1/2 (so we save time)
+        vector<int> tempTable((innerHashTable.empty()||(innerHashTable.size()==1)) ? innerHashTable.size() + 1 : pow(innerHashTable.size(), 2));
+        
+        bool rebuild = false;
+        do {
+            fill(tempTable.begin(), tempTable.end(), INT_MAX);
+            if (rebuild) {
+                a = rand() % bigPrime;
+                b = rand() % bigPrime;
+            }
+            rebuild = false;
+            for (auto it = innerHashTable.begin(); it != innerHashTable.end(); it++)
+            {
+                if (*it != INT_MAX)
+                {
+                    int h = hash(a, *it, b, bigPrime, int(tempTable.size()));
+                    if (tempTable[h] == INT_MAX)
+                    {
+                        tempTable[h] = *it;
+                    } else { //collision
+                        rebuild = true;
+                        break;
+                    }
+                    
+                } // else next step
+                
+            }
+            
+            int h = hash(a, elem, b, bigPrime, int(tempTable.size()));
+            if (tempTable[h] == INT_MAX)
+            {
+                tempTable[h] = elem;
+            } else { //collision
+                rebuild = true;
+            }
+            
+        } while (rebuild);
+        
+        innerHashTable = tempTable;
+    }
+    
+    void show() const {
+        for (auto it = innerHashTable.begin(); it != innerHashTable.end(); it++) {
+            if (*it == INT_MAX) {
+                cout<< "* ";
+            } else {
+                cout<< *it <<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    // returns element at given key
+    // if not found returns INT_MAX
+    int find(int key) const {
+        if (!innerHashTable.size()) {
+            return INT_MAX;
+        }
+        int innerHash = hash(a,key,b,bigPrime,innerHashTable.size());
+        return innerHashTable[innerHash];
+    }
+    
+};
+
+class PerfectHashTable {
+    int A, B;
+    vector<Bucket> outerHashTable;
+    const int bigPrime = 1009;
+    int sumAllBucketSizes = 0;
+    
+    void buildTable(const vector<int> &values, int capacityConstant){
+        do
+        {
+            sumAllBucketSizes = 0;
+            A = rand() % bigPrime;
+            B = rand() % bigPrime;
+            for (auto it = values.begin(); it != values.end(); it++)
+            {
+                int h = hash(A, *it, B, bigPrime, outerHashTable.size());
+                outerHashTable[h].add(*it);
+            }
+            for (auto it = outerHashTable.begin(); it != outerHashTable.end(); it++)
+                sumAllBucketSizes += it->size();
+            
+        // rebuild outer table each time sumAllBucketSizes > capacityConstant * values.size()
+        // just so if we're really unlucky and a lot of elements hash to the same bucket we don't have a huge waist of space
+        } while (sumAllBucketSizes > capacityConstant * values.size());
+    }
+    
+public:
+    // ratio == values.size() / outerHashTable.size()
+    // capacityConstant determins how big of a waist of memory we are ok with (smaller -> more rebuilds -> more time, but less size); used in buildTable
+    PerfectHashTable(const vector<int> &values, int capacityConstant = 10, double ratio = 0.5) {
+        int tableSize = findNextPrime(ratio * double(values.size()));
+        outerHashTable.resize(tableSize);
+        buildTable(values, capacityConstant);
+        
+    }
+    
+    // returns INT_MAX if error occured
+    int find(int key) const {
+        int outerHash = hash(A, key, B, bigPrime, outerHashTable.size());
+        int elem = outerHashTable[outerHash].find(key);
+        if (elem == INT_MAX) {
+            cout<< "ERROR: Element not found" << endl;
+        }
+        return elem;
+    }
+    
+    void show(){
+        for (int i = 0; i < outerHashTable.size(); i++){
+            cout<< i <<": ";
+            outerHashTable[i].show();
+        }
+    }
+    
+};
+
+int main(){
+    vector<int> a;
+    for (int i = 0; i<10; i++) {
+        a.push_back(rand()%100);
+    }
+    
+    PerfectHashTable b(a);
+    b.show();
+    
+    cout<< b.find(42);
+    
+    return 0;
+}
